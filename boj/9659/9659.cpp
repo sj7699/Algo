ae25beae1f9cda89865746e68e4480c68c4f1529
sj7699 <tkdwo7699@gmail.com>
2022-05-10 09:00:23 +0900

    9659 boj

---
+++ boj/9659/9659.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+    long long N;
+    scanf("%lli",&N);
+    if(N%2==0){
+        printf("CY");
+    }
+    else{
+        printf("SK");
+    }
+}
