35fd6a5ca0f24271ea525252071708ef0d668b7c
sj7699 <tkdwo7699@gmail.com>
2022-09-03 20:39:42 +0900

    2026 boj

--- boj/25025/25025.cpp
+++ boj/25025/25025.cpp
@@ -9,26 +9,30 @@ using namespace std;
 void fast_io() {
   cin.tie(0)->sync_with_stdio(0);
 }
-long long N,P;
-long long arr[1001];
+bool chk[150];
 int main(){
     fast_io();
-    cin>>N>>P;
-    for(int x=N;x>=0;x--){
-        long long input;
-        cin>>input;
-        if(x==0){
-            cout<<input%P<<'\n';
-        }
-        arr[x%(P-1)] += input;
+    int N;
+    string s;
+    cin>>N>>s;
+    chk['y']=true;
+    chk['i']=true;
+    chk['u']=true;
+    chk['o']=true;
+    chk['p']=true;
+    chk['h']=true;
+    chk['k']=true;
+    chk['j']=true;
+    chk['l']=true;
+    chk['b']=true;
+    chk['n']=true;
+    chk['m']=true;
+    if(N==1){
+        cout<<0;
+        return 0;
     }
-    for(long long x=1;x<P;x++){
-        long long temp=0;
-        long long temp2=1;
-        for(long long y=0;y<P;y++){
-            temp+=arr[y]*temp2;
-            temp2=(temp2*x)%P;
-        }
-        cout<<temp%P<<'\n';
+    else{
+        if(chk[s[N-2]] && !chk[s[N-1]]) cout<<1;
+        else cout<<0;
     }
 }
